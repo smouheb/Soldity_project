70c36a7a58beeabef9ab1715d233cc91c1fefc90
Smouheb <smael.mouheb@gmail.com>
2022-04-22 16:50:08 +0200

    Cleaning up the project

--- component/Layout.js
+++ component/Layout.js
@@ -4,17 +4,13 @@ import {Menu, Input} from 'semantic-ui-react';
 export default props => {
   return (
     <Menu>
-      <Menu.Item
-          name='home'
-        />
-      <Menu.Item
-            name='campaign'
-          />
+      <Menu.Item name='home'/>
+      <Menu.Item name='campaign'/>
       <Menu.Item header> Our Company </Menu.Item>
       <Menu.Menu position="right">
-        <Menu.Item>
-          <Input icon='search' placeholder='Search...' />
-        </Menu.Item>
+      <Menu.Item>
+        <Input icon='search' placeholder='Search...' />
+      </Menu.Item>
       </Menu.Menu>
     </Menu>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {Card, Button} from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import Layout from '../component/Layout';
